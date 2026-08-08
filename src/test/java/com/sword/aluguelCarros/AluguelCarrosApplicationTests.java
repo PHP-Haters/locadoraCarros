32d48b4beb891145a4d58e983164e846be617f72
Ana Ripoli <anaripoli.eng@gmail.com>
2026-08-08 13:24:33 -0300

    Primeiro commit inicializando o sistema

---
+++ src/test/java/com/sword/aluguelCarros/AluguelCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sword.aluguelCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
